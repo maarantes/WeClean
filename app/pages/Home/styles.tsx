e04bd6eb78197f67ad6b10857732b79f08cc7549
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2025-02-21 10:47:29 -0300

    [FRONT] feat: Adicionando botão de criar tarefa na tela Home

--- app/pages/Home/styles.tsx
+++ app/pages/Home/styles.tsx
@@ -2,14 +2,33 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
 
+    // Mover isso depois
+    botao_adicionar: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 12,
+        paddingVertical: 12,
+        borderWidth: 1, 
+        borderColor: "#5A189A",
+        borderStyle: "solid",
+        borderRadius: 4,
+        marginBottom: 40
+    },
+
+    botao_adicionar_texto: {
+        color: "#5A189A",
+        fontWeight: 600,
+        fontSize: 16
+    },
+
     container_sem_tarefa: {
-        minWidth: 100,
         borderWidth: 1, 
         borderColor: "#C4C4C4", 
         borderStyle: "solid",
         paddingVertical: 12,
         alignItems: "center",
-        borderRadius: 8
+        borderRadius: 4
     },
 
     texto_sem_tarefa: {
