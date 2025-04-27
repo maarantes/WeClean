992dc508f91c6e6a668257652170ad37e5e01584
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2025-04-27 19:45:15 -0300

    [BACK] fix: Consertando lógica de apagar o grupo pessoal

--- backend/services/grupos/apagarGrupoSozinho.tsx
+++ backend/services/grupos/apagarGrupoSozinho.tsx
@@ -1,29 +1,25 @@
-import { auth, db } from "../shared/firebaseConfigApp";
+import { db } from "../shared/firebaseConfigApp";
 import { doc, getDoc, collection, query, where, getDocs, deleteDoc } from "firebase/firestore";
 import { excluirTarefa } from "../tarefas/excluirTarefa";
 
-export const apagarGrupoSozinho = async (): Promise<void> => {
-  const uid = auth.currentUser?.uid;
-  if (!uid) return;
-
-  const grupoRef = doc(db, "Grupos", uid);
+export const apagarGrupoSozinho = async (grupoId: string): Promise<void> => {
+  const grupoRef = doc(db, "Grupos", grupoId);
   const grupoSnap = await getDoc(grupoRef);
-
   if (!grupoSnap.exists()) return;
 
   const grupoData = grupoSnap.data();
 
   if (grupoData.integrantes.length === 1) {
     const tarefasRef = collection(db, "Tarefas");
-    const q = query(tarefasRef, where("grupoId", "==", uid));
+    const q = query(tarefasRef, where("grupoId", "==", grupoId));
     const tarefasSnapshot = await getDocs(q);
 
     const deletarTarefasPromises = tarefasSnapshot.docs.map(async (docTarefa) => {
-      await excluirTarefa(docTarefa.id); // usa o excluirTarefa que limpa Tarefas + Calendário
+      await excluirTarefa(docTarefa.id);
     });
 
     await Promise.all(deletarTarefasPromises);
 
-    await deleteDoc(grupoRef); // Depois de limpar tudo, deleta o grupo
+    await deleteDoc(grupoRef);
   }
 }
